31100ee5a26a546bae61a87c6e4cd9f161e024b8
Lina Qiu <lq437@nyu.edu>
2017-11-07 12:36:58 -0500

    Second Version(some react)

---
+++ DynamicMenu.js
@@ -0,0 +1,9 @@
+
+function displayDropDownList() {
+  if(document.getElementById("mobileNav").style.display === "none") {
+    document.getElementById("mobileNav").style.display = "block";
+  }
+  else {
+    document.getElementById("mobileNav").style.display = "none";
+  }
+}
