1b4dc65eff1ddf9dca123088f7e97f256847b7e8
Lina Qiu <lq437@nyu.edu>
2019-09-19 18:41:34 -0400

    Update

--- DynamicMenu.js
+++ DynamicMenu.js
@@ -7,3 +7,31 @@ function displayDropDownList() {
     document.getElementById("mobileNav").style.display = "none";
   }
 }
+
+function hide() {
+  if(document.getElementById("mobileNav").style.display === "block" ) {
+    document.getElementById("mobileNav").style.display = "none";
+  }
+  if (window.innerWidth >= 900) {
+    document.getElementById("selfImage").align = "left";
+    document.getElementById("selfImage").style.width = "300px";
+    document.getElementById("selfImage").style.margin = "0px 50px 700px -50px";
+    document.getElementById("homeIntro").style.margin = "0px 0px 70px 0px";
+    // document.getElementById("researchInterests").style.margin = "0px 0px 0px 0px";
+  }
+  else {
+    document.getElementById("selfImage").align = "middle";
+    document.getElementById("selfImage").style.width = "250px";
+    document.getElementById("selfImage").style.margin = "0 auto";
+    document.getElementById("homeIntro").style.margin = "10px auto";
+  }
+}
+
+function align() {
+  if (window.innerWidth >= 900) {
+    document.getElementById("selfImage").align = "left";
+  }
+  else {
+    document.getElementById("selfImage").align = "middle";
+  }
+}
