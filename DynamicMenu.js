61da8bc40699223f400fc949be4346ab27b70d8b
Lina Qiu <lq437@nyu.edu>
2019-09-19 19:09:19 -0400

    javascript

--- DynamicMenu.js
+++ DynamicMenu.js
@@ -1,3 +1,16 @@
+//
+// if (window.innerWidth >= 900) {
+//   document.getElementById("selfImage").align = "left";
+//   document.getElementById("selfImage").style.width = "300px";
+//   document.getElementById("selfImage").style.margin = "0px 50px 700px -50px"
+// }
+// else {
+//   // document.getElementById("selfImage").align = "middle";
+//   document.getElementById("selfImage").style.width = "250px";
+//   document.getElementById("selfImage").style.height = "auto";
+//   document.getElementById("selfImage").style.display = "block";
+//   document.getElementById("selfImage").style.margin = "0 auto";
+// }
 
 function displayDropDownList() {
   if(document.getElementById("mobileNav").style.display === "none") {
@@ -8,10 +21,7 @@ function displayDropDownList() {
   }
 }
 
-function hide() {
-  if(document.getElementById("mobileNav").style.display === "block" ) {
-    document.getElementById("mobileNav").style.display = "none";
-  }
+function size() {
   if (window.innerWidth >= 900) {
     document.getElementById("selfImage").align = "left";
     document.getElementById("selfImage").style.width = "300px";
@@ -22,16 +32,16 @@ function hide() {
   else {
     document.getElementById("selfImage").align = "middle";
     document.getElementById("selfImage").style.width = "250px";
+    document.getElementById("selfImage").style.height = "auto";
+    document.getElementById("selfImage").style.display = "block";
     document.getElementById("selfImage").style.margin = "0 auto";
     document.getElementById("homeIntro").style.margin = "10px auto";
   }
 }
 
-function align() {
-  if (window.innerWidth >= 900) {
-    document.getElementById("selfImage").align = "left";
+function resize() {
+  if(document.getElementById("mobileNav").style.display === "block" ) {
+    document.getElementById("mobileNav").style.display = "none";
   }
-  else {
-    document.getElementById("selfImage").align = "middle";
-  }
+  size();
 }
